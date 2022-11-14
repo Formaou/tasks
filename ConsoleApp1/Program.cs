49a2454b1901b2c3dfe29623cf6f538558a5ee3d
Formaou <formaonk@gmail.com>
2022-11-14 17:03:05 +0300

    add task №6

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int imagesInRow = 3;
+            int imagesInAlbum = 52;
+
+            int fullRowsWithImages = imagesInAlbum / imagesInRow;
+            int overMeasure = imagesInAlbum % imagesInRow;
+
+            Console.WriteLine("В вашем альбоме {0} рядов, {1} картинок сверх меры", fullRowsWithImages, overMeasure);
+        }
+    }
+}
